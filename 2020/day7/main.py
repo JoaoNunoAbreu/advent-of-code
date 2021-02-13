aa0a01f686d6b0f871f0693d72dbdb6f30e3d47f
JoaoNunoAbreu <joao.nuno.cga@gmail.com>
2021-02-13 22:24:30 +0000

    dia 7

--- 2020/day7/main.py
+++ 2020/day7/main.py
@@ -1,10 +1,10 @@
 import re,json
 
-file = open('input2.txt', 'r')
+file = open('input.txt', 'r')
 lines = file.readlines()
 
-#print("Part: ",end="")
-#part = input()
+print("Part: ",end="")
+part = input()
 
 d = {}
 
@@ -16,42 +16,48 @@ for i in lines:
         j = j.replace("bags","").replace("bag","").strip()
         quant = re.search('(.*?)(?= )', j).group()
         tipo = re.search('(?<= )(.*)', j).group()
-        if(quant == "no"):
-            quant = 0
-            tipo = "N/A"
-        vals_fixed.append({
-            "quant": quant,
-            "tipo": tipo
-        })
+        if(quant != "no"):
+            vals_fixed.append({
+                "quant": int(quant),
+                "tipo": tipo
+            })
         
     d[k] = vals_fixed
 
-# Fazer função recursiva que leva como argumentos o dict e lista de bags que contêm shiny gold
 
-alterou = True
-matches = []
-while(alterou):
-    for key,value in d.items():
-        for v in value:
-            if(v['tipo'] == 'shiny gold'):
-                matches.append(key)
-    alterou = False
+if(part == "1"):
+    matches = []
+    while(True):
+        tmp = matches.copy()
+        for key,value in d.items():
+            for v in value:
+                if((v['tipo'] in matches or v['tipo'] == 'shiny gold') and key not in matches):
+                    matches.append(key)
+                    
+        # Se não houve alterações termina
+        if(tmp == matches):
+            break
 
-print(matches)
-#print(json.dumps(d,indent=4,sort_keys=True))
+    print(len(matches))
 
-#if(part == "1"):
-#    pass
-#
-#elif(part == "2"):
-#    pass
-#        
-#else:
-#    print("Parte inválida!")
+elif(part == "2"):
 
-def find_colors(d,l):
-    for key,value in d.items():
-        for v in value:
-            if(v['tipo'] == 'shiny gold'):
-                matches.append(key)
+    def conta(bag_collection, bag_name):
+        count = 0
+        top_level_bag = bag_collection[bag_name]
+
+        if len(top_level_bag) == 0:
+            return count
+        else:
+            for current_bag in top_level_bag:
+                count += current_bag['quant']
+                res = conta(bag_collection, current_bag["tipo"])
+                count += res * current_bag['quant']
+
+        return count
     
+    print(conta(d,'shiny gold'))
+
+else:
+    print("Parte inválida!")
+        
