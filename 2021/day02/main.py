f3df726af4c9672a7f1b9d72949e0a2e74d2429c
JoaoNunoAbreu <joao.nuno.cga@gmail.com>
2021-12-02 20:35:49 +0000

    update

---
+++ 2021/day02/main.py
@@ -0,0 +1,55 @@
+import sys
+
+
+def part1(lines):
+    depth = 0
+    horizontal = 0
+    for i in lines:
+        operation, units = i.split()
+        units = int(units)
+        if(operation == "down"):
+            depth += units
+        elif(operation == "up"):
+            depth -= units
+        elif(operation == "forward"):
+            horizontal += units
+
+    return depth * horizontal
+
+
+def part2(lines):
+    depth = 0
+    horizontal = 0
+    aim = 0
+    for i in lines:
+        operation, units = i.split()
+        units = int(units)
+        if(operation == "down"):
+            aim += units
+        elif(operation == "up"):
+            aim -= units
+        elif(operation == "forward"):
+            horizontal += units
+            depth = aim * units
+
+    return depth * horizontal
+
+
+def main():
+    file = open('input2.txt', 'r')
+    lines = file.readlines()
+    lines = [line.strip() for line in lines]
+
+    print("Part: ", end="")
+    part = input()
+
+    if(part == "1"):
+        print(part1(lines))
+    elif(part == "2"):
+        print(part2(lines))
+    else:
+        print("Parte inválida!")
+
+
+if __name__ == "__main__":
+    main()
