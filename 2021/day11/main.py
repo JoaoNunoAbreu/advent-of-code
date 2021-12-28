1878d4d823382557531e35712d8af7ce674c09da
JoaoNunoAbreu <joao.nuno.cga@gmail.com>
2021-12-28 17:21:01 +0000

    update

--- 2021/day11/main.py
+++ 2021/day11/main.py
@@ -1,9 +1,5 @@
 # Funções Auxiliares ----------------------------------------------------------
 
-def print_matrix(matrix):
-    for line in matrix:
-        print(line)
-
 
 def convert_to_matrix(lines):
     matrix = []
@@ -12,80 +8,183 @@ def convert_to_matrix(lines):
     return matrix
 
 
-def flash(matrix, x, y):
+def flash(matrix, x, y, flashed_points):
     width = len(matrix[0])
     height = len(matrix)
 
-    matrix[y][x] = 0
-
     if(x == 0 and y == 0):
-        matrix[y+1][x] += 1
-        matrix[y][x+1] += 1
-        matrix[y+1][x+1] += 1
+        if((x, y+1) not in flashed_points):
+            matrix[y+1][x] += 1
+        if((x+1, y) not in flashed_points):
+            matrix[y][x+1] += 1
+        if((x+1, y+1) not in flashed_points):
+            matrix[y+1][x+1] += 1
     elif(x == width-1 and y == 0):
-        matrix[y+1][x] += 1
-        matrix[y][x-1] += 1
-        matrix[y+1][x-1] += 1
+        if((x, y+1) not in flashed_points):
+            matrix[y+1][x] += 1
+        if((x-1, y) not in flashed_points):
+            matrix[y][x-1] += 1
+        if((x-1, y+1) not in flashed_points):
+            matrix[y+1][x-1] += 1
     elif(x == 0 and y == height-1):
-        matrix[y-1][x] += 1
-        matrix[y][x+1] += 1
-        matrix[y-1][x+1] += 1
+        if((x, y - 1) not in flashed_points):
+            matrix[y-1][x] += 1
+        if((x+1, y) not in flashed_points):
+            matrix[y][x+1] += 1
+        if((x+1, y-1) not in flashed_points):
+            matrix[y-1][x+1] += 1
     elif(x == width-1 and y == height-1):
-        matrix[y-1][x] += 1
-        matrix[y][x-1] += 1
-        matrix[y-1][x-1] += 1
+        if((x, y - 1) not in flashed_points):
+            matrix[y-1][x] += 1
+        if((x-1, y) not in flashed_points):
+            matrix[y][x-1] += 1
+        if((x-1, y-1) not in flashed_points):
+            matrix[y-1][x-1] += 1
     elif(x == 0):
-        matrix[y-1][x] += 1
-        matrix[y][x+1] += 1
-        matrix[y+1][x] += 1
-        matrix[y+1][x+1] += 1
+        if((x, y - 1) not in flashed_points):
+            matrix[y-1][x] += 1
+        if((x+1, y) not in flashed_points):
+            matrix[y][x+1] += 1
+        if((x, y+1) not in flashed_points):
+            matrix[y+1][x] += 1
+        if((x+1, y+1) not in flashed_points):
+            matrix[y+1][x+1] += 1
+        if((x+1, y-1) not in flashed_points):
+            matrix[y-1][x+1] += 1
     elif(x == width-1):
-        matrix[y-1][x] += 1
-        matrix[y][x-1] += 1
-        matrix[y+1][x] += 1
-        matrix[y+1][x-1] += 1
+        if((x, y - 1) not in flashed_points):
+            matrix[y-1][x] += 1
+        if((x-1, y) not in flashed_points):
+            matrix[y][x-1] += 1
+        if((x, y+1) not in flashed_points):
+            matrix[y+1][x] += 1
+        if((x-1, y+1) not in flashed_points):
+            matrix[y+1][x-1] += 1
+        if((x-1, y-1) not in flashed_points):
+            matrix[y-1][x-1] += 1
     elif(y == 0):
-        matrix[y+1][x] += 1
-        matrix[y][x-1] += 1
-        matrix[y+1][x-1] += 1
-        matrix[y+1][x+1] += 1
+        if((x, y+1) not in flashed_points):
+            matrix[y+1][x] += 1
+        if((x-1, y) not in flashed_points):
+            matrix[y][x-1] += 1
+        if((x+1, y) not in flashed_points):
+            matrix[y][x+1] += 1
+        if((x-1, y+1) not in flashed_points):
+            matrix[y+1][x-1] += 1
+        if((x+1, y+1) not in flashed_points):
+            matrix[y+1][x+1] += 1
     elif(y == height-1):
-        matrix[y-1][x] += 1
-        matrix[y][x-1] += 1
-        matrix[y-1][x-1] += 1
-        matrix[y-1][x+1] += 1
+        if((x, y - 1) not in flashed_points):
+            matrix[y-1][x] += 1
+        if((x-1, y) not in flashed_points):
+            matrix[y][x-1] += 1
+        if((x+1, y) not in flashed_points):
+            matrix[y][x+1] += 1
+        if((x-1, y-1) not in flashed_points):
+            matrix[y-1][x-1] += 1
+        if((x+1, y-1) not in flashed_points):
+            matrix[y-1][x+1] += 1
     else:
-        matrix[y-1][x] += 1
-        matrix[y][x-1] += 1
-        matrix[y+1][x] += 1
-        matrix[y+1][x-1] += 1
-        matrix[y+1][x+1] += 1
-        matrix[y-1][x+1] += 1
+        if((x, y - 1) not in flashed_points):
+            matrix[y-1][x] += 1
+        if((x-1, y) not in flashed_points):
+            matrix[y][x-1] += 1
+        if((x+1, y) not in flashed_points):
+            matrix[y][x+1] += 1
+        if((x, y+1) not in flashed_points):
+            matrix[y+1][x] += 1
+        if((x-1, y+1) not in flashed_points):
+            matrix[y+1][x-1] += 1
+        if((x+1, y+1) not in flashed_points):
+            matrix[y+1][x+1] += 1
+        if((x-1, y-1) not in flashed_points):
+            matrix[y-1][x-1] += 1
+        if((x+1, y-1) not in flashed_points):
+            matrix[y-1][x+1] += 1
 
+
+def replace_above_nine_with_zero(matrix, flashed_points):
+    for y in range(len(matrix)):
+        for x in range(len(matrix[y])):
+            if(matrix[y][x] > 9 and (x, y) in flashed_points):
+                matrix[y][x] = 0
+    return matrix
+
+
+def step(matrix):
+    flashed_points = []
+    for y in range(len(matrix)):
+        for x in range(len(matrix[y])):
+            matrix[y][x] += 1
+            if(matrix[y][x] > 9):
+                flash(matrix, x, y, flashed_points)
+                flashed_points.append((x, y))
+    return flashed_points
+
+
+def step_without_increment(matrix, flashed_points):
+    for y in range(len(matrix)):
+        for x in range(len(matrix[y])):
+            if(matrix[y][x] > 9 and (x, y) not in flashed_points):
+                flash(matrix, x, y, flashed_points)
+                flashed_points.append((x, y))
+    return flashed_points
+
+
+def search_for_suitable_flash(matrix, flashed_points):
+    for y in range(len(matrix)):
+        for x in range(len(matrix[y])):
+            if(matrix[y][x] > 9 and (x, y) not in flashed_points):
+                flash(matrix, x, y, flashed_points)
+                flashed_points.append((x, y))
+    return flashed_points
 
 # Part 1 ----------------------------------------------------------------------
 
 
 def part1(lines):
     matrix = convert_to_matrix(lines)
-    for y in range(len(matrix)):
-        for x in range(len(matrix[y])):
-            if(matrix[y][x] == 9):
-                flash(matrix, x, y)
-            else:
-                matrix[y][x] += 1
+    res = 0
+    for _ in range(100):
+        flashed_points = step(matrix)
+        replace_above_nine_with_zero(matrix, flashed_points)
 
-    print_matrix(matrix)
+        # Repeat until no more flashes are available
+        old_flashes = []
+        while(old_flashes != flashed_points):
+            old_flashes = [i for i in flashed_points]
+            flashed_points = search_for_suitable_flash(matrix, flashed_points)
 
+        replace_above_nine_with_zero(matrix, flashed_points)
+        res += len(flashed_points)
+    return res
 
 # Part 2 ----------------------------------------------------------------------
 
+
 def part2(lines):
-    pass
+    matrix = convert_to_matrix(lines)
+    res = 0
+    size_of_matrix = len(matrix) * len(matrix[0])
+    for i in range(int(9e99)):
+        flashed_points = step(matrix)
+        replace_above_nine_with_zero(matrix, flashed_points)
+
+        # Repeat until no more flashes are available
+        old_flashes = []
+        while(old_flashes != flashed_points):
+            old_flashes = [i for i in flashed_points]
+            flashed_points = search_for_suitable_flash(matrix, flashed_points)
+
+        replace_above_nine_with_zero(matrix, flashed_points)
+        if(len(flashed_points) == size_of_matrix):
+            res = i
+            break
+    return res+1
 
 
 def main():
-    file = open('input3.txt', 'r')
+    file = open('input.txt', 'r')
     lines = file.readlines()
     lines = [line.strip() for line in lines]
 
