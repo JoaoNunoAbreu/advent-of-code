951636032dad2a485e9f5eca34d0d4614b37c66b
João Abreu <jna@joaoabreu-2.Home>
2022-02-13 20:35:28 +0000

    update

--- 2021/day16/main.py
+++ 2021/day16/main.py
@@ -11,30 +11,43 @@ def bin_to_dec(bin_num):
     return int(bin_num, 2)
 
 
+def process_one_packet(bin_num):
+    print("bin_num single:", bin_num)
+    version = bin_to_dec(bin_num[:3])
+    type_id = bin_to_dec(bin_num[3:6])
+    rest = bin_num[6:]
+    parts = []
+    i = 0
+    while(rest[i] == "1"):
+        parts.append(rest[i+1:i+5])
+        i += 5
+    parts.append(rest[i+1:i+5])
+    tail = rest[i+5:]
+    return version, type_id, parts, tail
+
+
 def process_bin(bin_num):
     print("bin_num:", bin_num)
     version = bin_to_dec(bin_num[:3])
     type_id = bin_to_dec(bin_num[3:6])
     print("version, type_id:", version, type_id)
 
-    if(type_id == 4):
-        rest = bin_num[6:]
-        parts = []
-        i = 0
-        while(rest[i] == "1"):
-            parts.append(rest[i+1:i+5])
-            i += 5
-        parts.append(rest[i+1:i+5])
-        tail = rest[i+5:]
-        print(version, type_id, parts, tail)
-        return bin_to_dec("".join(parts))
+    length_type_id = bin_num[6]
+    if(length_type_id == "0"):
+        # com length dos packets todos
+        length = bin_to_dec(bin_num[7:7+15])
+        sub_packets = bin_num[7+15:7+15+length]
+        tail = bin_num[7+15+length:]
+        print(version, type_id, length, sub_packets, tail)
+        # ainda falta acabar isto
     else:
-        length_type_id = bin_num[6]
-        if(length_type_id == 0):
-            length = bin_to_dec(bin_num[7:7+15])
-            print("length:", length)
-        else:
-            print("length_type_id == 1")
+        # contem n sub packets
+        number_of_sub_packets = bin_to_dec(bin_num[7:7+11])
+        sub_tail = bin_num[7+11:]
+        for i in range(number_of_sub_packets):
+            sub_version, sub_type_id, sub_parts, sub_tail = process_one_packet(
+                sub_tail)
+            print(sub_version, sub_type_id, sub_parts, sub_tail)
 
 # Part 1 ----------------------------------------------------------------------
 
@@ -51,7 +64,7 @@ def part2(lines):
 
 
 def main():
-    filename = 'sub_packets.txt'
+    filename = '3_sub_packets.txt'
     file = open(filename, 'r')
     lines = file.readlines()
     lines = [line.strip() for line in lines]
