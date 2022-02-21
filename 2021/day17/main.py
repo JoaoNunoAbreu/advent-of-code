1dc3692b3ca154191aa9e3ad0d96e995bcdfc3cf
JoaoNunoAbreu <joao.nuno.cga@gmail.com>
2022-02-21 22:57:57 +0000

    update

--- 2021/day17/main.py
+++ 2021/day17/main.py
@@ -1,5 +1,4 @@
 import re
-''' import matplotlib.pyplot as plt '''
 
 # Funções Auxiliares ----------------------------------------------------------
 
@@ -12,85 +11,76 @@ def process_input(lines):
     return int(xi), int(xf), int(yi), int(yf)
 
 
-def calculate_range(xi, xf, yi, yf):
-    res = []
-    for x in range(xi, xf + 1):
-        for y in range(yi, yf + 1):
-            res.append((x, y))
-    return res
-
-
-def is_inside_area(x, y, area):
-    return (x, y) in area
-
-
-def calculate_trajectory(vel_x, vel_y, area, min_y):
+def calculate_trajectory(initial_vel_x, initial_vel_y, area):
     pos_x, pos_y = 0, 0
-    res = []
-    for _ in range(30):
+    max_y = -9e99
+    vel_x = initial_vel_x
+    vel_y = initial_vel_y
+    while True:
+
+        # Next position
         pos_x += vel_x
         pos_y += vel_y
-        res.append((pos_x, pos_y))
-        if(is_inside_area(pos_x, pos_y, area)):
+
+        # Drag and gravity adjustments
+        if vel_x > 0:
+            vel_x -= 1
+        elif vel_x < 0:
+            vel_x += 1
+        else:
+            vel_x = 0
+        vel_y -= 1
+
+        # Stop unnecessary calculations
+        if pos_x > area[1]:
             break
-        if(pos_y < min_y):
+        if pos_y < area[2] or pos_y > 10000:
             break
-        vel_x = vel_x - 1 if vel_x > 0 else vel_x + 1
-        vel_y = vel_y - 1
 
-    return res
+        # Check if it's the highest point
+        if(pos_y > max_y):
+            max_y = pos_y
+
+        # Check if we've reached the area
+        if area[0] <= pos_x <= area[1] and area[2] <= pos_y <= area[3]:
+            return max_y
+
+    # In case we didn't reach the area
+    return None
 
 
-def get_max_y_from_trajectory(trajectory):
-    max_y = -9e99
-    for i in trajectory:
-        max_y = max(max_y, i[1])
-    return max_y
-
-
-def test_possibilities(area, vel_x_min, vel_x_max, vel_y_min, vel_y_max):
-    max_y = -9e99
-    min_y = min(area, key=lambda x: x[1])[1]
-    count = 0
+def test_possibilities(area):
+    vel_x_min = 1
+    vel_x_max = area[1]
+    vel_y_min = -10000
+    vel_y_max = 10000
+    d = {}
+    res = -9e99
     for i in range(vel_x_min, vel_x_max + 1):
         for j in range(vel_y_min, vel_y_max + 1):
-            trajectory = calculate_trajectory(i, j, area, min_y)
-            if(is_inside_area(trajectory[-1][0], trajectory[-1][1], area)):
-                max_y_in_trajectory = get_max_y_from_trajectory(trajectory)
-                if(max_y_in_trajectory > max_y):
-                    max_y = max_y_in_trajectory
-                #plot_graph(area, trajectory)
-            print(count, "/", (vel_x_max - vel_x_min + 1)
-                  * (vel_y_max - vel_y_min + 1))
-            count += 1
-    return max_y
+            max_y = calculate_trajectory(i, j, area)
+            if(max_y != None):
+                d[(i, j)] = max_y
+                if(max_y > res):
+                    res = max_y
+    return res, d
 
 
-''' def plot_graph(area, trajectory):
-    x = [i[0] for i in area]
-    y = [i[1] for i in area]
-    x1 = [i[0] for i in trajectory]
-    y1 = [i[1] for i in trajectory]
-    plt.scatter(x, y, color='blue')
-    plt.scatter(x1, y1, color='red')
-    plt.show() '''
+def exercise(lines):
+    area = process_input(lines)
+    return test_possibilities(area)
 
 # Part 1 ----------------------------------------------------------------------
 
 
-def part1(lines):
-    xi, xf, yi, yf = process_input(lines)
-    area = calculate_range(xi, xf, yi, yf)
-    #trajectory = calculate_trajectory(6, 9, area)
-    #plot_graph(area, trajectory)
-    return test_possibilities(area, 1, 200, -200, 200)
+def part1(res):
+    return res[0]
 
 
 # Part 2 ----------------------------------------------------------------------
 
-
-def part2(lines):
-    pass
+def part2(res):
+    return len(res[1])
 
 
 def main():
@@ -99,8 +89,9 @@ def main():
     lines = file.readlines()
     lines = [line.strip() for line in lines]
 
-    print("Part 1:", part1(lines))
-    print("Part 2:", part2(lines))
+    res = exercise(lines)
+    print("Part 1:", part1(res))
+    print("Part 2:", part2(res))
 
 
 if __name__ == "__main__":
