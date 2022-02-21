1df0267459b723155fbc8f78a7adde29e2ec889a
JoaoNunoAbreu <joao.nuno.cga@gmail.com>
2022-02-21 17:06:27 +0000

    update

--- 2021/day17/main.py
+++ 2021/day17/main.py
@@ -1,11 +1,40 @@
+import re
+import matplotlib.pyplot as plt
+
 # Funções Auxiliares ----------------------------------------------------------
 
+
+def process_input(lines):
+    line = lines[0]
+    line = line[13:]
+    xi, xf = re.search('(?<=x=)(.*?)(?=,)', line).group().split('..')
+    yi, yf = re.search('(?<=y=)(.*)', line).group().split('..')
+    return int(xi), int(xf), int(yi), int(yf)
+
+
+def calculate_range_of_values(xi, xf, yi, yf):
+    res = []
+    for x in range(xi, xf + 1):
+        for y in range(yi, yf + 1):
+            res.append((x, y))
+    return res
+
+
+def draw(res):
+    x = [i[0] for i in res]
+    y = [i[1] for i in res]
+    plt.scatter(x, y)
+    plt.show()
 
 # Part 1 ----------------------------------------------------------------------
 
 
 def part1(lines):
-    pass
+    xi, xf, yi, yf = process_input(lines)
+    res = calculate_range_of_values(xi, xf, yi, yf)
+    draw(res)
+    return len(res)
+
 
 # Part 2 ----------------------------------------------------------------------
 
@@ -15,7 +44,7 @@ def part2(lines):
 
 
 def main():
-    filename = 'input.txt'
+    filename = 'input2.txt'
     file = open(filename, 'r')
     lines = file.readlines()
     lines = [line.strip() for line in lines]
