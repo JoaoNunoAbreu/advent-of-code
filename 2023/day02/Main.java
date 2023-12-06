6823a5cde05dc2a24c72d00b2f2e6cef06807f06
João Nuno Abreu <43481430+JoaoNunoAbreu@users.noreply.github.com>
2023-12-06 20:10:19 +0000

    Create Main.java

---
+++ 2023/day02/Main.java
@@ -0,0 +1,84 @@
+package day02;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static utils.Utils.readFile;
+
+public class Main {
+    private static final String DAY_FILE = "/Users/abreuj/dev/general/advent-of-code/2023/day02/input.txt";
+    private static final Map<String, Integer> LIMITS = Map.of(
+            "red", 12, "green", 13, "blue", 14
+    );
+
+    private static List<String[]> process(List<String> data) {
+        return data.stream()
+                .map(line -> line.replaceAll("Game \\d+: ", ""))
+                .map(line -> line.split(";"))
+                .map(line -> Arrays.stream(line).map(String::trim).toArray(String[]::new))
+                .collect(Collectors.toList());
+    }
+
+    // ---------------------------------------------------------------------------------------------------------
+    public static int part1(List<String> data) {
+        List<String[]> processed = process(data);
+
+        int i = 1;
+        int res = 0;
+        for (String[] lines : processed) {
+            boolean invalid = false;
+            for (String line : lines) {
+                String[] pairs = line.split(", ");
+                for (String pair : pairs) {
+                    String[] singlePair = pair.split(" ");
+                    int number = Integer.parseInt(singlePair[0]);
+                    String color = singlePair[1];
+                    if (number > LIMITS.get(color)) {
+                        invalid = true;
+                    }
+                }
+            }
+            if (!invalid) {
+                res += i;
+            }
+            i++;
+        }
+        return res;
+    }
+
+    // ---------------------------------------------------------------------------------------------------------
+    public static int part2(List<String> data) {
+        List<String[]> processed = process(data);
+
+        int res = 0;
+        for (String[] lines : processed) {
+            Map<String, Integer> colors = new HashMap<>();
+            for (String line : lines) {
+                String[] pairs = line.split(", ");
+                for (String pair : pairs) {
+                    String[] singlePair = pair.split(" ");
+                    int number = Integer.parseInt(singlePair[0]);
+                    String color = singlePair[1];
+                    colors.compute(color, (key, existingNumber) -> existingNumber == null
+                            ? number
+                            : Math.max(existingNumber, number)
+                    );
+                }
+            }
+            res += colors.values().stream().reduce(1, (a, b) -> a * b);
+        }
+        return res;
+    }
+
+    // ---------------------------------------------------------------------------------------------------------
+
+    public static void main(String[] args) {
+        List<String> data = readFile(DAY_FILE);
+        System.out.println("Part 1: " + part1(data));
+        System.out.println("Part 2: " + part2(data));
+    }
+}
+
