c113a3eca60add153419ff278d8efe932e8c1cd1
JoaoNunoAbreu <joao.nuno.cga@gmail.com>
2023-12-06 23:48:04 +0000

    creating day 3

---
+++ 2023/day03/Main.java
@@ -0,0 +1,30 @@
+package day03;
+
+import java.util.List;
+
+import static utils.Utils.readFile;
+
+public class Main {
+    private static final String DAY_FILE = System.getProperty("user.dir") + "/2023/day03/input.txt";
+
+    // ---------------------------------------------------------------------------------------------------------
+
+    public static int part1(List<String> data) {
+        return 0;
+    }
+
+    // ---------------------------------------------------------------------------------------------------------
+
+    public static int part2(List<String> data) {
+        return 0;
+    }
+
+    // ---------------------------------------------------------------------------------------------------------
+
+    public static void main(String[] args) {
+        List<String> data = readFile(DAY_FILE);
+        System.out.println("Part 1: " + part1(data));
+        System.out.println("Part 2: " + part2(data));
+    }
+}
+
