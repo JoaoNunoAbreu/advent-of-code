b7001774eac55a515123e58c734669850fc3efa7
João Nuno Abreu <43481430+JoaoNunoAbreu@users.noreply.github.com>
2023-12-06 01:45:34 +0000

    Update Main.java

--- 2023/day01/Main.java
+++ 2023/day01/Main.java
@@ -1,27 +1,73 @@
 package day01;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Map;
+import java.util.Objects;
+
+import static java.lang.Character.isDigit;
+import static utils.FileReader.readFile;
+import static utils.FileReader.reverseString;
 
 public class Main {
-    public static List<String> readFile(String filename) {
-        List<String> data = new ArrayList<>();
-        try (Scanner myReader = new Scanner(new File(filename))) {
-            while (myReader.hasNextLine()) {
-                data.add(myReader.nextLine());
+    private static final String DAY_FILE = "/Users/abreuj/dev/general/advent-of-code/2023/day01/input.txt";
+    private static final Map<String, Integer> numbers = Map.of(
+            "one", 1, "two", 2, "three", 3, "four", 4, "five", 5, "six", 6, "seven", 7, "eight", 8, "nine", 9
+    );
+    private static final Map<String, Integer> reversedNumbers = Map.of(
+            "eno", 1, "owt", 2, "eerht", 3, "ruof", 4, "evif", 5, "xis", 6, "neves", 7, "thgie", 8, "enin", 9
+    );
+
+    // ---------------------------------------------------------------------------------------------------------
+
+    public static int part1(List<String> data) {
+        return data.stream()
+                .map(line -> line.replaceAll("\\s*[a-z]\\s*", ""))
+                .map(line -> line.charAt(0) + String.valueOf(line.charAt(line.length() - 1)))
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+
+    // ---------------------------------------------------------------------------------------------------------
+    public static String iterator(String line){
+        int begin = 0;
+        while (begin < line.length()) {
+            for (int i = begin; i < line.length(); i++) {
+                if(i == begin && isDigit(line.charAt(i))){
+                    return String.valueOf(line.charAt(i));
+                }
+
+                String word = line.substring(begin, i + 1);
+                if (numbers.containsKey(word)) {
+                    return numbers.get(word).toString();
+                }
+                if (reversedNumbers.containsKey(word)) {
+                    return reversedNumbers.get(word).toString();
+                }
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            begin++;
         }
-        return data;
+        return line;
+    }
+
+    public static String converter(String line) {
+        String first = iterator(line);
+        String second = iterator(reverseString(line));
+        return first + second;
+    }
+
+    public static int part2(List<String> data) {
+        return data.stream()
+                .map(Main::converter)
+                .map(line -> line.replaceAll("\\s*[a-z]\\s*", ""))
+                .filter(Objects::nonNull)
+                .map(line -> line.charAt(0) + String.valueOf(line.charAt(line.length() - 1)))
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 
     public static void main(String[] args) {
-        String dayFile = System.getProperty("user.dir") + "/2023/day01/input.txt";
-        List<String> data = readFile(dayFile);
-        System.out.println(data);
+        List<String> data = readFile(DAY_FILE);
+        System.out.println("Part 1: " + part1(data));
+        System.out.println("Part 2: " + part2(data));
     }
 }
