de3dfe436e6f5637dec861855c30b9f61f4baa13
JoaoNunoAbreu <joao.nuno.cga@gmail.com>
2024-11-04 17:52:41 +0000

    update

--- 2023/utils/Utils.java
+++ 2023/utils/Utils.java
@@ -5,14 +5,15 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Utils {
 
     private Utils() {}
 
     public static List<String> readFile(String filename) {
-        try {
-            return Files.lines(Paths.get(filename)).collect(Collectors.toList());
+        try (Stream<String> lines = Files.lines(Paths.get(filename))) {
+            return lines.toList();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
