b7f08a0b47db22857cda4e247f69e4b22c44a6eb
JoaoNunoAbreu <joao.nuno.cga@gmail.com>
2024-12-05 13:04:59 +0000

    add 2024 day 1

---
+++ 2024/main.py
@@ -0,0 +1,42 @@
+INPUT_FILE = 'input.txt'
+
+def read_file():
+    with open(INPUT_FILE, 'r') as file:
+        return [x.strip() for x in file.readlines()]
+
+def process_data(data):
+    return [[int(x) for x in line.split('   ')] for line in data]
+
+def part_1(data):
+    arr1 = [x[0] for x in data]
+    arr2 = [x[1] for x in data]
+
+    arr1.sort()
+    arr2.sort()
+
+    return sum([abs(x-y) for x,y in zip(arr1, arr2)])
+
+def part_2(data):
+    arr1 = [x[0] for x in data]
+    arr2 = [x[1] for x in data]
+
+    arr1.sort()
+    arr2.sort()
+
+    map1 = {x:arr1.count(x) for x in arr1}
+    map2 = {x:arr2.count(x) for x in arr2}
+
+    return sum([x * map2.get(x, 0) for x in arr1])
+    
+
+def main():
+    data = read_file()
+    processed_data = process_data(data)
+    part1 = part_1(processed_data)
+    part2 = part_2(processed_data)
+
+    print("Part 1:", part1)
+    print("Part 2:", part2)
+
+if __name__ == '__main__':
+    main()
